fbb8471da8d12782c3b0ca8f313422952de8961d
DatLe <datltse130318@fpt.edu.vn>
2020-10-12 14:44:50 +0700

    Fixed Login Signup Screen

--- lib/screens/Welcome/login.dart
+++ lib/screens/Welcome/login.dart
@@ -2,31 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hci_booking_pt/components/brand_label.dart';
 import 'package:hci_booking_pt/components/rounded_button.dart';
 import 'package:hci_booking_pt/components/rounded_input.dart';
+import 'package:hci_booking_pt/components/rounded_password.dart';
 import 'package:hci_booking_pt/components/screen_with_background.dart';
 import 'package:hci_booking_pt/screens/welcome/components/form_title.dart';
 import 'package:hci_booking_pt/theme/colors.dart';
 
-int _groupValue = -1;
-
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key key}) : super(key: key);
-  Widget _myRadioButton({String title, int value, Function onChanged}) {
-    return RadioListTile(
-      value: value,
-      activeColor: MainColors.kMain,
-      groupValue: _groupValue,
-      onChanged: onChanged,
-      title: Text(title,
-          style: const TextStyle(
-          color:  Colors.white,
-        fontWeight: FontWeight.bold,
-        fontFamily: "ProductSans",
-        fontStyle:  FontStyle.normal,
-        fontSize: 16.0
-    ),),
-
-    );
-  }
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -41,55 +23,62 @@ class RegisterScreen extends StatelessWidget {
             FormTitle(title: "Login"),
             Column(
               children: [
-                MobileInputField(
-                  hintText: "Enter Mobile No.",
+                RoundedInputField(
+                  hintText: "Username",
                 ),
-                FullNameInput(
-                  hintText: "Enter Full Name",
-                ),
-                EmailInput(
-                  hintText: "Enter Email",
-                ),
-                PasswordInput(
+                RoundedPasswordField(
                   hintText: "Password",
-                ),
-                MobileInputField(
-                  hintText: "Confirm Password",
+                  icon: Icons.lock,
                 ),
                 RoundedButton(
-                  text: "REGISTER",
+                  text: "Login",
                 ),
               ],
             ),
-
-
-
+            Column(children: [
+              Text("Forget Password ?",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "ProductSans",
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16),
+                  textAlign: TextAlign.center),
+              Container(
+                child: Text("OR",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 16),
+                    textAlign: TextAlign.center),
+              ),
+            ]),
             Container(
-              color: Colors.blue,
-              child: Column(
+              margin: EdgeInsets.symmetric(vertical: 30),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:<Widget> [
-                    _myRadioButton(
-                      title: "A",
-                      value: 0,
-                      // onChanged: (newValue) => setState(() => _groupValue = newValue),
-                    ),
-                  _myRadioButton(
-                    title: "FEMALE",
-                    value: 0,
-                    // onChanged: (newValue) => setState(() => _groupValue = newValue),
-                  ),
+                children: [
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      child: Icon(
+                        Icons.circle_notifications,
+                        size: 70,
+                        color: Colors.blue,
+                      )),
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      child: Icon(
+                        Icons.add_a_photo,
+                        size: 70,
+                        color: MainColors.kMain,
+                      )),
                 ],
               ),
             ),
-
-
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Already have an account?"),
-                Text("Login", style: TextStyle(color: MainColors.kMain)),
+                Text("Don't have an account ?  "),
+                Text("Register", style: TextStyle(color: MainColors.kMain)),
               ],
             )
           ],
---
+++ lib/screens/welcome/register.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:hci_booking_pt/components/brand_label.dart';
+import 'package:hci_booking_pt/components/rounded_button.dart';
+import 'package:hci_booking_pt/components/rounded_input.dart';
+import 'package:hci_booking_pt/components/rounded_password.dart';
+import 'package:hci_booking_pt/components/screen_with_background.dart';
+import 'package:hci_booking_pt/screens/welcome/components/form_title.dart';
+import 'package:hci_booking_pt/theme/colors.dart';
+
+
+
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MainColors.kDark,
+      body: ScreenWithBackground(
+        pathBackground: "asset/images/background/1.jpg",
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BrandLabel(),
+            FormTitle(title: "Login"),
+            Column(
+              children: [
+                RoundedInputField(
+                  hintText: "Enter Mobile No.",
+                ),
+                RoundedInputField(
+                  hintText: "Enter Full Name",
+                ),
+                RoundedInputField(
+                  hintText: "Enter Email",
+                  icon: Icons.email,
+                ),
+                RoundedPasswordField(
+                  hintText: "Password",
+                  ishaveVisibleButton: false,
+                ),
+                RoundedPasswordField(
+                  hintText: "Confirm Password",
+                  ishaveVisibleButton: false,
+                ),
+                RoundedButton(
+                  text: "REGISTER",
+                  
+                ),
+              ],
+            ),
+
+
+
+            Divider(),           
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Already have an account?"),
+                Text("Login", style: TextStyle(color: MainColors.kMain)),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
