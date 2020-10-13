715ef4f0af372870452425c6f0b28c2ae7946e80
DatLe <datltse130318@fpt.edu.vn>
2020-10-13 10:06:42 +0700

    Add interact bottom bar

---
+++ lib/screens/user_screen/user_notification.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserNotification extends StatelessWidget {
+  const UserNotification({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Notification"),
+    );
+  }
+}
--- lib/screens/user_screen/user_screen.dart
+++ lib/screens/user_screen/user_screen.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:hci_booking_pt/screens/user_screen/components/container_rounded.dart';
+import 'package:hci_booking_pt/screens/user_screen/find_pt.dart';
+import 'package:hci_booking_pt/screens/user_screen/profile.dart';
+import 'package:hci_booking_pt/screens/user_screen/user_home.dart';
+import 'package:hci_booking_pt/screens/user_screen/user_notification.dart';
 import 'package:hci_booking_pt/theme/colors.dart';
+//Supported class
+class UserScreenItemDTO
+{
+  String title;
+  Widget screen;
+  UserScreenItemDTO(this.title,this.screen);
+
+}
 
 class UserScreen extends StatefulWidget {
-  Widget child;
-  String title;
-  UserScreen({Key key, this.child, this.title}) : super(key: key);
+  UserScreen({Key key}) : super(key: key);
   @override
   _UserScreenState createState() => _UserScreenState();
 }
 
+
 class _UserScreenState extends State<UserScreen> {
+  int _currentindex = 0;
+  final _tabs = [
+    UserScreenItemDTO("Home",UserHome()),
+    UserScreenItemDTO("Search",FindPT()),
+    UserScreenItemDTO("Notification",UserNotification()),
+    UserScreenItemDTO("Profile",Profile()),
+  ];
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: MainColors.kDark,
-        // leading: BackButton(color: MainColors.kLight),
-        title: Text(this.widget.title),
+        title: Text(_tabs[_currentindex].title),
         centerTitle: true,
       ),
       backgroundColor: MainColors.kDark,
       body: ContainerRounded(
-        child: this.widget.child,
+        child: _tabs[_currentindex].screen,
       ),
       bottomNavigationBar: Container(
           decoration: BoxDecoration(
@@ -31,6 +47,12 @@ class _UserScreenState extends State<UserScreen> {
                   Border(top: BorderSide(color: MainColors.kMain, width: 3))),
           padding: EdgeInsets.only(top: 10),
           child: BottomNavigationBar(
+            currentIndex: _currentindex,
+            onTap: (index){
+              setState(() {
+                 _currentindex = index;
+              });
+            },
             unselectedItemColor: MainColors.kLight,
             type: BottomNavigationBarType.fixed,
             backgroundColor: MainColors.kDark,
@@ -39,7 +61,7 @@ class _UserScreenState extends State<UserScreen> {
               BottomNavigationBarItem(
                   label: "Search", icon: Icon(Icons.search)),
               BottomNavigationBarItem(
-                  label: "Hello", icon: Icon(Icons.fitness_center)),
+                  label: "Notification", icon: Icon(Icons.notifications)),
               BottomNavigationBarItem(
                   label: "Account", icon: Icon(Icons.account_circle_outlined)),
             ],
