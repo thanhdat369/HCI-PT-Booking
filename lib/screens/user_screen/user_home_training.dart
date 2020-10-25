cf5674027303de306fdf99038d52a89e7f539a11
Triệu Minh Huy (K13) <huytmse130336@fpt.edu.vn>
2020-10-25 11:09:29 +0700

    add action into endnow button

--- lib/screens/user_screen/user_home_training.dart
+++ lib/screens/user_screen/user_home_training.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hci_booking_pt/components/rounded_button_size.dart';
 import 'package:hci_booking_pt/components/square_image.dart';
+import 'package:hci_booking_pt/screens/user_screen/confirm_end_course.dart';
 import 'package:hci_booking_pt/screens/user_screen/rounder_training_day.dart';
 import 'package:hci_booking_pt/theme/colors.dart';
 
@@ -12,44 +13,42 @@ class UserHomeTraining extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      child: Column(
-        children: [
-          Info_CheckOut(),
-            const Divider(
-            color: Colors.white,
-            height: 10,
-            thickness: 1,
-            indent: 10,
-            endIndent: 10,
-          ),
-          
-
-          SizedBox(
-                height: size.height * 0.41,
-                child: Info_Exercise(),
+        child: Column(
+      children: [
+        Info_CheckOut(),
+        const Divider(
+          color: Colors.white,
+          height: 10,
+          thickness: 1,
+          indent: 10,
+          endIndent: 10,
+        ),
+        SizedBox(
+          height: size.height * 0.41,
+          child: Info_Exercise(),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 0.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "This course will end a 20 November, 2019",
+                style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w700,
+                    color: MainColors.kSoftLight),
               ),
-
-          Container(
-            margin: EdgeInsets.only(top: 0.5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text("This course will end a 20 November, 2019", style: TextStyle(fontSize: 10,fontWeight: FontWeight.w700,
-                color: MainColors.kSoftLight),),
-              
-              RoundedButtonSize(text: "End Now", height:24, width: 80,),
-
-              
-              ],
-            ),
+              RoundedButtonSize(
+                  text: "End Now",
+                  height: 24,
+                  width: 80,
+                  press: () => ConfirmEndCourse().showAlertDialog(context))
+            ],
           ),
-            
-            
-          
-          
-        ],
-      )
-    );
+        ),
+      ],
+    ));
   }
 }
 
@@ -64,20 +63,22 @@ class Info_CheckOut extends StatelessWidget {
           padding: EdgeInsets.only(left: 10),
           alignment: Alignment.topLeft,
           child: Text(
-                      "Training with",
-                      style: const TextStyle(
-                          color: const Color(0xffffffff),
-                          fontWeight: FontWeight.w700,
-                          fontFamily: "NotoSans",
-                          fontStyle: FontStyle.normal,
-                          fontSize: 18),
-                    ),
+            "Training with",
+            style: const TextStyle(
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w700,
+                fontFamily: "NotoSans",
+                fontStyle: FontStyle.normal,
+                fontSize: 18),
+          ),
         ),
         Container(
           padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 7),
           child: Row(
             children: [
-              SquareImage(size: 110,),
+              SquareImage(
+                size: 110,
+              ),
               Container(
                 height: 130,
                 margin: EdgeInsets.only(left: 15),
@@ -93,16 +94,23 @@ class Info_CheckOut extends StatelessWidget {
                           fontStyle: FontStyle.normal,
                           fontSize: 20),
                     ),
-                    
-                    RoundedButtonSize(text: "Send Message", width: 170, height: 30,),
-
+                    RoundedButtonSize(
+                      text: "Send Message",
+                      width: 170,
+                      height: 30,
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(bottom: 3),
-                      child: Text("Meeting will start in 30 minutes", style: const TextStyle(fontSize: 10),),
+                      child: Text(
+                        "Meeting will start in 30 minutes",
+                        style: const TextStyle(fontSize: 10),
+                      ),
                     ),
-
-                    RoundedButtonSize(text: "Join Meeting", width: 170, height: 30,),
-
+                    RoundedButtonSize(
+                      text: "Join Meeting",
+                      width: 170,
+                      height: 30,
+                    ),
                   ],
                 ),
               ),
@@ -119,35 +127,85 @@ class Info_Exercise extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-
-          Text("Week 1", style: TextStyle(fontSize: 17,fontWeight: FontWeight.w700,
-              color: MainColors.kSoftLight),),
-          RoundedTrainingDay(is_finish: true,),
-          RoundedTrainingDay(title: "Day 2 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 3 - Upper Body Workout", num_of_excercise: 5,),
-        
-          Text("Week 2", style: TextStyle(fontSize: 17,fontWeight: FontWeight.w700,
-              color: MainColors.kSoftLight),),
-          RoundedTrainingDay(title: "Day 4 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 5 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 6 - Upper Body Workout", num_of_excercise: 5,),
-
-          Text("Week 3", style: TextStyle(fontSize: 17,fontWeight: FontWeight.w700,
-              color: MainColors.kSoftLight),),
-          RoundedTrainingDay(title: "Day 7 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 8 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 9 - Upper Body Workout", num_of_excercise: 5,),
-
-          Text("Week 4", style: TextStyle(fontSize: 17,fontWeight: FontWeight.w700,
-              color: MainColors.kSoftLight),),
-          RoundedTrainingDay(title: "Day 10 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 11 - Full Leg Workout", num_of_excercise: 6,),
-          RoundedTrainingDay(title: "Day 12 - Upper Body Workout", num_of_excercise: 5,),
-        ],
+      scrollDirection: Axis.vertical,
+      children: [
+        Text(
+          "Week 1",
+          style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+              color: MainColors.kSoftLight),
+        ),
+        RoundedTrainingDay(
+          is_finish: true,
+        ),
+        RoundedTrainingDay(
+          title: "Day 2 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 3 - Upper Body Workout",
+          num_of_excercise: 5,
+        ),
+        Text(
+          "Week 2",
+          style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+              color: MainColors.kSoftLight),
+        ),
+        RoundedTrainingDay(
+          title: "Day 4 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 5 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 6 - Upper Body Workout",
+          num_of_excercise: 5,
+        ),
+        Text(
+          "Week 3",
+          style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+              color: MainColors.kSoftLight),
+        ),
+        RoundedTrainingDay(
+          title: "Day 7 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 8 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 9 - Upper Body Workout",
+          num_of_excercise: 5,
+        ),
+        Text(
+          "Week 4",
+          style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+              color: MainColors.kSoftLight),
+        ),
+        RoundedTrainingDay(
+          title: "Day 10 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 11 - Full Leg Workout",
+          num_of_excercise: 6,
+        ),
+        RoundedTrainingDay(
+          title: "Day 12 - Upper Body Workout",
+          num_of_excercise: 5,
+        ),
+      ],
     );
   }
 }
