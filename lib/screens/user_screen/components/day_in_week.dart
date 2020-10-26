b9ca60c52d3315844b15c1952779183f154bf081
Lê Thành Đạt <53030386+thanhdat369@users.noreply.github.com>
2020-10-26 12:35:57 +0700

    Dat lt 03 (#18)
    * Add Interact Login and fix Register radiobuton
    
    * Finish Exercise Screen
    
    * mergemain
    
    * Update rounded_button.dart
    
    * Fix video screen and booking screen
    
    * Booking screen
    
    Booking screen
    
    * Edit Video Screen
    
    * Update DayInWeek chosing day
    
    Co-authored-by: DatLe <datltse130318@fpt.edu.vn>

--- lib/screens/user_screen/components/day_in_week.dart
+++ lib/screens/user_screen/components/day_in_week.dart
@@ -10,19 +10,26 @@ class DayInWeek extends StatefulWidget {
 
 class _DayInWeekState extends State<DayInWeek> {
   var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  var active_index = 0;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height*0.1,
+      height: size.height * 0.1,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: days.length,
         itemBuilder: (BuildContext context, int index) {
           return DayInWeekItem(
             text: days[index],
+            isActivate: index == active_index,
             width: size.width * 0.101,
+            press: () {
+              setState(() {
+                active_index = index;
+              });
+            },
           );
         },
       ),
@@ -33,7 +40,14 @@ class _DayInWeekState extends State<DayInWeek> {
 class DayInWeekItem extends StatefulWidget {
   String text;
   double width;
-  DayInWeekItem({Key key, this.width = 40, this.text = "Mon"})
+  bool isActivate;
+  Function press;
+  DayInWeekItem(
+      {Key key,
+      this.width = 40,
+      this.text = "Mon",
+      this.isActivate = false,
+      this.press})
       : super(key: key);
 
   @override
@@ -45,14 +59,12 @@ class _DayInWeekItemState extends State<DayInWeekItem> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          this.isClick = !isClick;
-        });
-      },
+      onTap: this.widget.press,
       child: Container(
         decoration: BoxDecoration(
-            color: MainColors.kSoftDark,
+            color: this.widget.isActivate
+                ? MainColors.kMain
+                : MainColors.kSoftDark,
             // borderRadius: BorderRadius.all(Radius.circular(100)),
             shape: BoxShape.circle,
             border: Border.all(width: 1, color: MainColors.kSoftLight)),
@@ -61,7 +73,8 @@ class _DayInWeekItemState extends State<DayInWeekItem> {
         padding: EdgeInsets.all(2),
         margin: EdgeInsets.only(right: 10, bottom: 10, top: 10),
         child: Text(
-          this.widget.text, style: TextStyle(fontSize: 13),
+          this.widget.text,
+          style: TextStyle(fontSize: 13),
         ),
       ),
     );
